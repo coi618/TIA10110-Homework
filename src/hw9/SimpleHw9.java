01f15c368f6ee4c8d3fb5d7bb846f69ec7c86d95
coi618 <coi618@hotmail.com>
2024-03-16 22:39:31 +0800

    Start develop hw9

---
+++ src/hw9/SimpleHw9.java
@@ -0,0 +1,24 @@
+package hw9;
+
+public class SimpleHw9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 快胃王比賽 */
+		mottohayaku();
+		/* 生活費 */
+		showMeMoney();
+	}
+	/* 快胃王比賽 */
+	private static void mottohayaku() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* 生活費 */
+	private static void showMeMoney() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
