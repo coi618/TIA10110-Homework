1f187a0719312e4b3387e18bda5175a3b20d6e70
coi618 <coi618@hotmail.com>
2024-06-17 13:39:32 +0800

    Finish hw10

--- src/hw10/SimpleHw10.java
+++ src/hw10/SimpleHw10.java
@@ -1,18 +1,22 @@
 package hw10;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Scanner;
-import java.text.Format;
-import java.text.DecimalFormat;
+import java.util.Set;
+import java.util.TreeSet;
+
 
 public class SimpleHw10 {
 
 	public static void main(String[] args) {
-//		System.out.println("Hw10-1: 請設計一隻程式，用亂數產生5個介於1~100之間的整數，"
-//				+ "\n\t而輸出結果可以判斷出這5個整數為是否為質數 (提示:Math類別)");
-//		fiveNumber();
+		System.out.println("Hw10-1: 請設計一隻程式，用亂數產生5個介於1~100之間的整數，"
+				+ "\n\t而輸出結果可以判斷出這5個整數為是否為質數 (提示:Math類別)");
+		fiveNumber();
 		
 		System.out.println("Hw10-2: 請設計一隻程式，讓使用者可以輸入一個任意數後，可以選擇要以下列何種表示方法顯示"
 				+ "\n\t(1)千分位 (2)百分比(3)科學記號，而輸入非任意數會顯示提示訊息如圖 "
@@ -22,15 +26,15 @@ public class SimpleHw10 {
 				+ "\n\t\t輸入123，千分位為123"
 				+ "\n\t\t輸入0.75，百分比為75%，"
 				+ "\n\t\t輸入1，結果為100%");
-//		numFormatter();
-		Scanner sc = new Scanner(System.in);
-		numFormatter2(sc); // 1) 使用 RE 判斷輸入。 2) Scanner 單一宣告
-//		
-//		System.out.println("Hw10-3: 請設計一隻程式，讓使用者輸入日期(年月日，例如:20110131)後，"
-//				+ "\n\t可以轉成想要的輸出格式化成(1) 年/月/日(2)月/日/年(3)日/月/年三選一，"
-//				+ "\n\t而輸入非指定日期數字格式會顯示出提示訊息如圖"
-//				+ "\n\t(提示:TestFormatter.java, Java API文件，判斷格式可用正規表示法)");
-//		calFormatter();
+		numFormatter();
+
+		System.out.println("Hw10-3: 請設計一隻程式，讓使用者輸入日期(年月日，例如:20110131)後，"
+				+ "\n\t可以轉成想要的輸出格式化成(1) 年/月/日(2)月/日/年(3)日/月/年三選一，"
+				+ "\n\t而輸入非指定日期數字格式會顯示出提示訊息如圖"
+				+ "\n\t(提示:TestFormatter.java, Java API文件，判斷格式可用正規表示法)");
+		calFormatter();
+		
+//		System.out.println("End of main()");
 	}
 	
 	/******************** For Hw10-1 ********************/
@@ -69,46 +73,28 @@ public class SimpleHw10 {
 		/* 2. 選擇格式 > 選擇檢查 */
 		/* 3. 輸出結果 */
 		
-		/* Test Case: 
-		 * ("abc", "數字格式不正確，請再輸入一次") 
-		 * ("123", "123, 12300%, 1.23E2")
-		 * ("-123", "-123, -12300%, -1.23E2")
-		 * ("1234567890", "1,234,567,890, 123456789000%, 1.23457E9")
-		 * ("0.123456789", "0.123456789, 12.3456789%, 1.23457E-1")
-		 * ("0", "0, 0%, 0E0")
-		 * (double MAX)
-		 * (double MIN)
-		 * (double )
-		 * */
-		/* test zoom */
-//		double num = 
-//				Double.MAX_VALUE; 	/* 1.7976931348623157E308 */
-//				Double.MIN_NORMAL; 	/* 2.2250738585072014E-308 */
-//				Double.MIN_VALUE; 	/* 4.9E-324 */
-//				Double.NaN; 		/* NaN -> kPlace: 非數值 */
-//				Double.NEGATIVE_INFINITY; /* -Infinity -> kPlace: -∞ */
-//				Double.POSITIVE_INFINITY; /* Infinity -> kPlace: ∞ */
-
 		double num = inputNum(); /* 1. 輸入數字 > 格式檢查 */
-		System.out.println("Input: " + num);
+//		System.out.println("Input: " + num);
 		
-//		int choose = inputChoose(); /* 2. 選擇格式 > 選擇檢查 */
-		int choose = 3;
-		/* test zoom */
+		int choose = inputChoose(); /* 2. 選擇格式 > 選擇檢查 */
+
 		resultPrint(num, choose); /* 3. 輸出結果 */
 		
-		System.out.println("End of numFormatter.");
+//		System.out.println("End of numFormatter.");
 	} /* END of numFormatter() */
 	
 	private static double inputNum() {
+		
 		while (true) {
 			System.out.print("請輸入數字: ");
 //			try (Scanner sc = new Scanner(System.in)) { /* Infinite Loop */
 			Scanner sc = new Scanner(System.in);
+			
 			if(sc.hasNextDouble()) {
 				return sc.nextDouble();
 			}
 			System.out.println("數字格式不正確，請再輸入一次!");
+			
 		}
 	} /* END of double inputNum() */
 	
@@ -147,24 +133,83 @@ public class SimpleHw10 {
 			/* Do nothing */
 		}
 	} /* END of void resultPrint() */
-	
-	/******************** For Hw10-2 ********************/
-	private static void numFormatter2(Scanner sc) { // 1) 使用 RE 判斷輸入。 2) Scanner 單一宣告
-		double num = inputNum2(sc);
-	}
-	
-	private static double inputNum2(Scanner sc) {
-//		while (true) {
-//			System.out.println("請輸入數字: ");
-//		}
-//		java.lang.Runtime rt = null;
-		java.lang.Runtime.getRuntime();
-		return 0.0;
-		
-	}
-		
+			
 	/******************** For Hw10-3 ********************/
 	private static void calFormatter() {
+		Date input = getDateInput(); // EX: 20240617
+		int choose = getChoose();	// 1 | 2 | 3
+		
+		printCal(input, choose);
+//		System.out.println("End of calFormatter()");
 		
 	} /* END of calFormatter() */
+
+	private static void printCal(Date input, int choose) {
+		Format mySfm1 = new SimpleDateFormat("yyyy/MM/dd");
+		Format mySfm2 = new SimpleDateFormat("MM/dd/yyyy");
+		Format mySfm3 = new SimpleDateFormat("dd/MM/yyyy");
+		switch (choose) {
+		case 1:	// (1) 年/月/日
+//			System.out.println("Choose: 1");
+			System.out.println(mySfm1.format(input));
+			break;
+		case 2: // (2)月/日/年
+//			System.out.println("Choose: 2");
+			System.out.println(mySfm2.format(input));
+			break;
+		case 3: // (3)日/月/年
+//			System.out.println("Choose: 3");
+			System.out.println(mySfm3.format(input));
+			break;
+		default:
+			System.out.println("default");
+		}
+		
+		
+	} // END of printCal()
+
+	private static int getChoose() {
+		String str = null;
+		int choose = 0;
+		String regChoose = "^[1-3]$";
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("欲格式化成(1) 年/月/日(2)月/日/年(3)日/月/年: ");
+			if (sc.hasNextLine()) {
+				str = sc.nextLine();
+				if (str.matches(regChoose)) {
+					choose = Integer.valueOf(str);
+					return choose;
+				}
+			}
+			System.out.println("請重新選擇");
+		}
+			
+	} // END of getChoose()
+
+	private static Date getDateInput() {
+		String str = null;
+		Date day = null; // util.Date
+		// 定義正則表達式，匹配格式為 yyyyMMdd 的日期 | SimpleDateFormat.setLenient(false), 就不需要了
+//  		String regDate = "^(\\d{4})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$";
+		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyyMMdd");
+		inputFormat.setLenient(false); 
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("請輸入日期(年月日，例如:20240615): ");
+			if (sc.hasNextLine()) { 
+				str = sc.nextLine();
+//				if (!str.matches(regDate)) continue; // continue | break
+				try {
+					day = inputFormat.parse(str);
+					return day;
+				} catch (ParseException pe) {
+					// Do nothing...
+				}
+			}
+        	System.out.println("日期格式不正確，請再輸入一次!");
+        }
+        
+//		return null;
+	} // END of getDateInput()
 }
